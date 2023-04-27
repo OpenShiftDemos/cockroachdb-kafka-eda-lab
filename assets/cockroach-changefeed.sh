80d1398ff279100a2bde1fe3246903178f1e6fae
Erik M Jacobs <erikmjacobs@gmail.com>
2023-04-27 16:07:14 -0400

    fix changefeed for new app test

--- assets/cockroach-changefeed.sh
+++ assets/cockroach-changefeed.sh
@@ -6,8 +6,8 @@ COCKROACH_DB=` oc get secret $COCKROACH_DB_SECRET -o json | jq -cr .data.options
 COCKROACH_DB_PASSWORD=`oc get secret $COCKROACH_DB_SECRET -o json | jq -cr .data.password | base64 -d`
 
 KAFKA_ROUTE=`oc get route crdb-cluster-kafka-external-bootstrap -n crdb-kafka -o json | jq -cr .spec.host`
-KAFKA_CERT_ORIG=`oc get kafka crdb-cluster -n crdb-kafka -o json | jq -cr .status.listeners[1].certificates[0]`
-KAFKA_CERT_BASE64=`echo $KAFKA_CERT_ORIG | base64 -w0 -`
+oc get kafka crdb-cluster -n crdb-kafka -o json | jq -cr .status.listeners[1].certificates[0] | sed '/^$/d' > /tmp/kafka-cert.txt
+KAFKA_CERT_BASE64=`cat /tmp/kafka-cert.txt | base64 -w0 -`
 USERNAME=`echo $WORKSHOP_VARS | jq -cr .user`
 
 echo "Use this password: $COCKROACH_DB_PASSWORD"
